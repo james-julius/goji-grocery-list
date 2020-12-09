44cc919b62969720b1263b49f6639347e2b59a8a
james-julius <jamesdarby7@gmail.com>
2020-12-08 17:56:34 -0800

    Passable stylling for now

--- grocery-list/src/components/GroceryListItem/GroceryListItem.js
+++ grocery-list/src/components/GroceryListItem/GroceryListItem.js
@@ -6,17 +6,25 @@ import './GroceryListItem.scss';
 export default function GroceryListItem({ item, deleteItem}) {
     return (
       <li className="list-item">
-        {item.name}
-        {item.currentlyHave ? (
-          <span className="stock-status have">Have</span>
-        ) : (
-          <span className="stock-status out">Ran out</span>
-        )}
-        <span className="last-updated">Last updated: {moment(item.lastUpdated).format('Do MMM')}</span>
-        <Link to={`/grocery-item/${item.id}`}>
-          <button className="item-link">View</button>
-        </Link>
-        <button className="delete" onClick={() => deleteItem(item.id)}>Delete</button>
+        <h6>{item.name}</h6>
+        <div class="status-container">
+          {item.currentlyHave ? (
+            <span className="stock-status have">HAVE</span>
+          ) : (
+            <span className="stock-status out">OUT</span>
+          )}
+          <span className="last-updated">
+            Last updated: <br /> {moment(item.lastUpdated).format("Do MMM")}
+          </span>
+        </div>
+        <div class="action-container">
+          <Link to={`/grocery-item/${item.id}`}>
+            <button className="item-link">View</button>
+          </Link>
+          <button className="delete" onClick={() => deleteItem(item.id)}>
+            Delete
+          </button>
+        </div>
       </li>
     );
 }
